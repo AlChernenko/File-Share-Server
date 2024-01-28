4c3f6b8c59994f47d38b0c2a25c9bdd49d6f4fd3
AlChernenko <138977021+AlChernenko@users.noreply.github.com>
2024-01-28 20:57:23 +0100

    I tried it 6 times
    Sometimes its works some time its not....

--- messeng client.py
+++ messeng client.py
@@ -1,44 +1,33 @@
+import socket
 import selectors
-import sys
-
-server_address = 'localhost'
-server_port = 5000
-
-sel = selectors.DefaultSelector()
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_address, server_port))
-client_socket.setblocking(False)
-sel.register(client_socket, selectors.EVENT_WRITE, data=None)
-
-
-def send_message(message):
-    message = message.encode()
-    client_socket.sendall(message)
-
-    sel.unregister(client_socket)
-    client_socket.close()
-
 
 def main():
-    sel.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE, data=None)
+    server_address = 'localhost'
+    server_port = 5000
+    sel = selectors.DefaultSelector()
 
-    while True:
-        message = input("Enter message: ")
-        send_message(message)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_address, server_port))
+        client_socket.setblocking(False)
+        sel.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE)
 
-        events = sel.select()
-        for key, mask in events:
-            if mask & selectors.EVENT_READ:
-                response = key.fileobj.recv(1024)
-                if response:
-                    print(response.decode())
-                else:
-                    print("Server closed the connection.")
-                    sel.unregister(client_socket)
-                    client_socket.close()
-                    break
+        while True:
+            message = input("> > >  ")
+            if message.lower() == 'o':
+                break
 
+            message = message.encode()
+            client_socket.sendall(message)
+
+            events = sel.select(timeout=1)
+            for key, mask in events:
+                if mask & selectors.EVENT_READ:
+                    response = key.fileobj.recv(1024)
+                    if response:
+                        print("Received:", response.decode())
+                    else:
+                        print("Server closed the connection.")
+                        return
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,65 +1,45 @@
 import socket
 import selectors
 
-default_selector = selectors.DefaultSelector()
-
-def find_available_port(start_port, end_port):
-    for port in range(start_port, end_port):
-        try:
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.bind(('localhost', 5000))
-            return server_socket, port
-        except OSError:
-            continue
-        return None, None
-            
-def server():
-
-    server_socket, port = find_available_port(5000, 5100)
-
-    if server_socket == None:
-        print("Available port is not found")
-        return
-    
-    server_socket.listen()
-    print(f"Server is running on port {port}")
-
-    
-    default_selector.register(fileobj=server_socket, events=selectors.EVENT_READ, data=accept_connection)
-
-
-def accept_connection(server_socket):
+def accept_connection(server_socket, sel):
     client_socket, client_address = server_socket.accept()
-    print("New connection from: ", client_address)
+    print("New connection from:", client_address)
+    client_socket.setblocking(False)
+    sel.register(client_socket, selectors.EVENT_READ, data=handle_client)
 
-def accepted_mes(client_socket):
+def handle_client(client_socket, sel):
     try:
         request = client_socket.recv(1024)
         if request:
-            print(request)
+            print("Received:", request.decode())
+            response = "Message received".encode()
+            client_socket.sendall(response)
         else:
-            print("Connection closed by client: ", client_socket.getpeername())
-            client_socket.send(b'Connection to server successfully')
-            default_selector.unregister(client_socket)
+            print("Connection closed by client:", client_socket.getpeername())
+            sel.unregister(client_socket)
             client_socket.close()
-    except ConnectionResetError:
-        print("Client forcibly closed the connection")
-        default_selector.unregister(client_socket)
-        client_socket.close()
-    except OSError as e:
-        print("Connection error:", e)
-        default_selector.unregister(client_socket)
+    except Exception as e:
+        print("Error:", e)
+        sel.unregister(client_socket)
         client_socket.close()
 
+def main():
+    server_address = 'localhost'
+    server_port = 5000
+    sel = selectors.DefaultSelector()
 
-def event_loop():
-    while True:
-        events = default_selector.select()
-        for key, _ in events:
-            callback = key.data
-            callback(key.fileobj)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((server_address, server_port))
+        server_socket.listen()
+        print(f"Server is running on {server_port}")
+        server_socket.setblocking(False)
+        sel.register(server_socket, selectors.EVENT_READ, data=accept_connection)
 
-if __name__ == '__main__':
-    print('Server run')
-    server()
-    event_loop()
+        while True:
+            events = sel.select(timeout=1)
+            for key, mask in events:
+                callback = key.data
+                callback(key.fileobj, sel)
+
+if __name__ == "__main__":
+    main()
