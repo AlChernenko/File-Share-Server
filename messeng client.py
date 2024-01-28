ab1c916903819f2f7e1d34bed39b1b7ba66087fd
Petr <shumilin123@yandex.ru>
2024-01-28 21:01:40 +0300

    none

---
+++ messeng client.py
@@ -0,0 +1,13 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создаем сокет
+sock.connect(('localhost', 5000))  # подключемся к серверному сокету
+
+while True:
+    mes = str(input("> > >  "))
+    if mes == '0':
+        break
+    sock.send(bytes(mes, encoding = 'UTF-8'))  # отправляем сообщение
+    
+sock.close()  # закрываем соединение
+
+
